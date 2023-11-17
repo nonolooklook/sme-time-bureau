70cd00a2aa0a34c7bfd429a26bd84ab2d21ed2e7
Aiden X <caichao.xu@gmail.com>
2023-11-17 17:14:56 +0800

    feat: update condiut key

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -1,5 +1,5 @@
 // export const NFTContractAddress = '0x560B65205dEA9E14bB169c91650915503c41928C'
 // export const NFTContractAddress = '0x6E6267A4D7196Cf98c8094723772c755eb4dC108'
-export const NFTContractAddress = '0x4bF272d6f6aD78afa1b0361E46eEA3B045bb2d40'
+export const NFTContractAddress = '0xdC6f975744546b7F3b2850B81b69a5084DB7B0Ad'
 
 export const TokenId = 1n
