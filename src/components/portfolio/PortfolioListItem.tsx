a93a28c448a9458d4f86866e9c5e091716f03abd
Aiden X <caichao.xu@gmail.com>
2023-11-25 10:53:53 +0800

    feat: update arb goerli contract

--- src/components/portfolio/PortfolioListItem.tsx
+++ src/components/portfolio/PortfolioListItem.tsx
@@ -1,5 +1,4 @@
 import { displayBalance } from '@/utils/display'
-import { parseUnits } from 'viem'
 import { useCancelList } from '@/hooks/useCancelList'
 import { Spinner } from '@/components/Spinner'
 
@@ -19,9 +18,9 @@ export const PortfolioListItem = ({
   const ps = order?.entry.parameters
   const csd = ps.consideration
   const offer = ps.offer
-  const count = parseUnits(isBid ? csd?.[0]?.startAmount : (offer?.[0]?.startAmount as `${number}`), 0)
-  const min = parseUnits(isBid ? offer?.[0]?.startAmount : (csd?.[0]?.startAmount as `${number}`), 0) / count
-  const max = parseUnits(isBid ? offer?.[0]?.endAmount : csd?.[0]?.endAmount, 0) / count
+  const count = BigInt(isBid ? csd?.[0]?.startAmount : offer?.[0]?.startAmount)
+  const min = BigInt(isBid ? offer?.[0]?.startAmount : csd?.[0]?.startAmount) / count
+  const max = BigInt(isBid ? offer?.[0]?.endAmount : csd?.[0]?.endAmount) / count
   const expected = min / 2n + max / 2n
   const { cancelList, isCancelLoading } = useCancelList(order, () => {
     mutate?.()
