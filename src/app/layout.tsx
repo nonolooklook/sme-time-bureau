18aab143ad817b9a2c205e3a4afa8491b892ff2d
Aiden X <caichao.xu@gmail.com>
2023-11-28 16:42:30 +0800

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat, Exo_2 } from 'next/font/google'
 import './global.css'
 import { Toaster } from 'sonner'
 import { ReactNode } from 'react'
+import Script from 'next/script'
 
 const montserrat = Montserrat({ subsets: ['latin'], weight: ['700', '600', '400', '300'] })
 
@@ -13,7 +14,22 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
+      <Script id='google-analytics'>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PWV6NH6J');
+        `}
+      </Script>
       <body className={montserrat.className}>
+        <Script id={'no-script'}>
+          {`
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWV6NH6J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        `}
+        </Script>
         <Toaster position={'top-right'} />
         <Providers>{children}</Providers>
       </body>
