cac096cc55e884ee07f3d4c385af9521f876b638
Aiden X <caichao.xu@gmail.com>
2023-11-29 23:29:48 +0800

    feat: add google

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,23 @@ import { ConnectKitButton } from 'connectkit'
 import Link from 'next/link'
 import { useAccount } from 'wagmi'
 import { usePathname } from 'next/navigation'
+import Script from 'next/script'
 
 export const Header = () => {
   const { address } = useAccount()
   const pathname = usePathname()
   return (
     <header className={'z-10 bg-black bg-opacity-40 text-white border-b border-b-[#DED1B630] fixed top-0 w-full'}>
+      <Script async src='https://www.googletagmanager.com/gtag/js?id=G-46NJNM8TCB'></Script>
+      <Script id={'google'}>
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-46NJNM8TCB');
+        `}
+      </Script>
       <div className='container mx-auto flex items-center justify-between'>
         <Link className='flex items-center gap-2' href={'/'}>
           <div className={'text-[24px] font-semibold'}>SME</div>
