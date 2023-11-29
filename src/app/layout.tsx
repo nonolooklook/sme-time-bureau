fdd3d8e13ea6059a1864f5a7312e272035df67f7
Aiden X <caichao.xu@gmail.com>
2023-11-29 15:13:32 +0800

    feat: add google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './global.css'
 import { Toaster } from 'sonner'
 import { ReactNode } from 'react'
 import Script from 'next/script'
+import Head from 'next/head'
 
 const montserrat = Montserrat({ subsets: ['latin'], weight: ['700', '600', '400', '300'] })
 
@@ -14,22 +15,17 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <Script id='google-analytics'>
+      <Script async src='https://www.googletagmanager.com/gtag/js?id=G-46NJNM8TCB'></Script>
+      <Script id={'google'}>
         {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-PWV6NH6J');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-46NJNM8TCB');
         `}
       </Script>
       <body className={montserrat.className}>
-        <Script id={'no-script'}>
-          {`
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWV6NH6J"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        `}
-        </Script>
         <Toaster position={'top-right'} />
         <Providers>{children}</Providers>
       </body>
